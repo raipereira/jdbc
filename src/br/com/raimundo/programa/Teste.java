ee8110e7fe2bab7597eba6b2bf84abb1d8d3b94c
raipereira <raipereira84@gmail.com>
2018-01-10 15:26:31 -0200

    Executando Statiment e lista Resultado

--- src/br/com/raimundo/programa/Teste.java
+++ src/br/com/raimundo/programa/Teste.java
@@ -2,7 +2,9 @@ package br.com.raimundo.programa;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Teste {
 	
@@ -10,10 +12,22 @@ public class Teste {
 		
 		Connection c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/loja-virtual", "SA", "");
 		
-		System.out.println("conecão aberta");
+		Statement stmt = c.createStatement();
+		boolean resultado = stmt.execute("select * from Produto");
+		if(resultado) {
+			ResultSet result = stmt.getResultSet();
+			while(result.next()) {
+				int id = result.getInt("id");
+				String nome = result.getString("nome");
+				String descricao = result.getString("descricao");
+				System.out.println(id + "\n" + nome + "\n" + descricao);
+			}
+			
+			result.close();
+			stmt.close();
+			c.close();
 		
-		c.close();
-		
+	}
 	}
 
 }
