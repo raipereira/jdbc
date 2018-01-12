5cabd37887e05d9f1868754fdd1e07b39df667a4
raipereira <raipereira84@gmail.com>
2018-01-12 13:35:04 -0200

    Tratar Injection com PrepareStentement

--- src/br/com/raimundo/programa/TestaInsercao.java
+++ src/br/com/raimundo/programa/TestaInsercao.java
@@ -1,6 +1,7 @@
 package br.com.raimundo.programa;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,10 +11,16 @@ public class TestaInsercao {
 	
 	public static void main(String[] args) throws SQLException {
 		
+		String nome = "Notebook";
+		String descricao = "Notebook 'i5";
+		
 		Connection connection = Database.getConection();
-		Statement stmt = connection.createStatement();	
-		boolean result = stmt.execute("insert into Produto(nome, descricao) values('Notebook', 'i5')",
-				Statement.RETURN_GENERATED_KEYS);
+		String sql = "insert into Produto(nome, descricao) values(?,?)";
+		PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);	
+		stmt.setString(1, nome);
+		stmt.setString(2, descricao);
+		
+		boolean result = stmt.execute();
 		System.out.println(result);
 		
 		ResultSet resultSet = stmt.getGeneratedKeys();
